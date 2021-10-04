74fc99cecbe0a8fcd7839f5c523f043b3e1cd63a
Ignacio Bianchi <bianchi.ignacio97@gmail.com>
2021-10-04 14:47:27 -0300

    Correcciones en el nombre del controller por otro mas representativo y detalles de la view del mismo

---
+++ TP4/Lab.MVC/Controllers/StarWarsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Web.Mvc;
+using Lab.MVC.Models;
+using Newtonsoft.Json;
+
+namespace Lab.MVC.Controllers
+{
+    public class StarWarsController : Controller
+    {
+
+        // GET: People
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    httpClient.BaseAddress = new Uri("https://swapi.dev/api/");
+                    var responseTask = await httpClient.GetStringAsync("people");
+
+                    var peopleResponse = JsonConvert.DeserializeObject<PeopleResponse>(responseTask);
+                    return View(peopleResponse);
+                }
+            }
+            catch (Exception ex)
+            {
+                return View("Error", ex);                
+            }
+            
+        }
+    }
+}

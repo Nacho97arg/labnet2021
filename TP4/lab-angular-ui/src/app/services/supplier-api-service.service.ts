ff6d56f91811f923fc2b8c3a6533dfbf72841ee8
Ignacio Bianchi <bianchi.ignacio97@gmail.com>
2021-10-15 20:00:10 -0300

    Se mejoro la interfaz.

--- TP4/lab-angular-ui/src/app/services/supplier-api-service.service.ts
+++ TP4/lab-angular-ui/src/app/services/supplier-api-service.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable,throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 import { environment } from 'src/environments/environment';
 import { Supplier } from '../models/supplier';
@@ -15,19 +16,28 @@ export class SupplierApiService {
   constructor(private http: HttpClient) { }
 
   public getAllSupliers() : Observable<Array<Supplier>>{
-    let url = environment.apiSuppliers + this.endPoint;
-    return this.http.get<Array<Supplier>>(url);
+      let url = environment.apiSuppliers + this.endPoint;
+      return this.http.get<Array<Supplier>>(url).pipe(
+        catchError(this.errorHandler));
   }
   public deleteSupplier(id:number): Observable<any>{
     let url = environment.apiSuppliers + this.endPoint + id.toString();
-    return this.http.delete(url);
+    return this.http.delete(url).pipe(
+      catchError(this.errorHandler));
   }
   public addSupplier(newSupplier: Supplier): Observable<any>{
     let url = environment.apiSuppliers + this.endPoint;
-    return this.http.put(url,newSupplier);
+    return this.http.put(url,newSupplier).pipe(
+      catchError(this.errorHandler));
   }
   public updateSupplier(modifiedSupplier: Supplier): Observable<any>{
     let url = environment.apiSuppliers + this.endPoint + modifiedSupplier.SupplierID.toString();
-    return this.http.put(url,modifiedSupplier);
+    return this.http.put(url,modifiedSupplier).pipe(
+      catchError(this.errorHandler));
+  }
+
+  private errorHandler(error : HttpErrorResponse){
+    console.error(error.error.ExceptionMessage);    
+    return throwError(error);
   }
 }
